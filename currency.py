3996d05245c70cdab288bf1f1467d94105ca8101
Dreamwave7 <dima190179@gmail.com>
2023-04-29 16:02:11 +0300

    qweqwer

--- currency.py
+++ currency.py
@@ -1,26 +1,35 @@
-from datetime import datetime
 import asyncio
-import aiohttp
-import sys
 import platform
+import sys
+from datetime import datetime
 
+import aiohttp
 
+separator = "========================================"
 
-async def main(currency:str, date:datetime = None):
+async def main(currency:str, date:datetime):
     async with aiohttp.ClientSession() as connect:
-        async with connect.get(f"https://api.privatbank.ua/p24api/exchange_rates?json&date=20.04.2023") as response:
+        async with connect.get(f"https://api.privatbank.ua/p24api/exchange_rates?json&date={date}") as response:
             result = await response.json()
-            await currency_parser(result, currency = currency)
+            await currency_parser(result, currency = currency, date=date)
 
 
-async def currency_parser(jsonfile:dict, currency:str, date = None):
+async def currency_parser(jsonfile:dict, currency:str, date):
      for currencies in jsonfile["exchangeRate"]:
          if currencies["currency"] == currency.upper():
-            print(f"Валюта : {currencies['currency']}\nКурс продажу НБУ: {currencies['saleRateNB']}\nКурс продажу ПриватБанку: {currencies['saleRate']}\nКурс купівлі ПриватБанку :{currencies['purchaseRate']}")
+            print(separator)
+            print(f"Поточна дата : {date}\nБазова валюта : {currencies['baseCurrency']}\nВалюта : {currencies['currency']}\nКурс продажу НБУ : {currencies['saleRateNB']}\nКурс продажу ПриватБанку : {currencies['saleRate']}\nКурс купівлі ПриватБанку : {currencies['purchaseRate']}")
+            print(separator)
 
+async def data_parser(days,currency="usd"):
+    currentDate = datetime.now().date()
+    date2 = currentDate.strftime("%d.%m.%Y")
+    await main(currency= currency,date = date2)
 
 if __name__== "__main__":
+    # curr = sys.argv[1]
     if platform.system() == 'Windows':
             asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-    r = asyncio.run(main("PLN"))
+    # r = asyncio.run(main(curr))
+    a = asyncio.run(data_parser(2))
